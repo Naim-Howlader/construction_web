f3e8f724e094a0ec18d35ec2c8125b95a3aa2763
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2023-06-02 12:46:22 +0600

    update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Gallery;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
     protected $table = "categories";
     protected $primaryKey = "category_id";
+
+    public function getGalleries()
+    {
+        return $this->hasMany(Gallery::class, 'category_id', 'category_id');
+    }
 }
